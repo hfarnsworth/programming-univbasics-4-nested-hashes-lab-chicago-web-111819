e7f62162d35e88fbccb2654818b850d080e2721c
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-11 08:07:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,8 +16,7 @@ def hopper
         }
      }
 
-programmer_hash[:grace_hopper]
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -38,6 +37,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:alan_kay][:known_for]
+  
 end
 
 def dennis_ritchies_language
@@ -56,6 +58,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:dennis_ritchie][:languages]
+     
 end
 
 def adding_matz
@@ -102,6 +107,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     
      
      
 end
